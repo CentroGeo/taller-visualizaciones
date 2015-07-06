4862dbe7c109bb5f87a106e03d1ac775806f3afe
tapiamcclung <rtapia@centrogeo.edu.mx>
2015-07-06 09:34:53 -0500

    updated examples of d3 radar charts with cartodb sql api

--- projects/functions.js
+++ projects/functions.js
@@ -1,44 +1,43 @@
 function main() {
-cartodb.createVis('map', 'https://rtapia.cartodb.com/api/v2/viz/d2673aee-f9a3-11e4-a29c-0e9d821ea90d/viz.json', {
-	//shareable: true,
-	title: true,
-	description: true,
-	//search: true,
-	tiles_loader: true,
-	center_lat: 19.411,
-	center_lon: -99.16,
-	zoom: 16
-})
-.done(function(vis, layers) {
-  // layer 0 is the base layer, layer 1 is cartodb layer
-  // setInteraction is disabled by default
-  /*layers[0].setInteraction(true);
-  layers[0].on('featureOver', function(e, latlng, pos, data) {
-	cartodb.log.log(e, latlng, pos, data);
-  });*/
-  // you can get the native map to work with it
-  var map = vis.getNativeMap();
-  // now, perform any operations you need
-  // map.setZoom(3);
-  // map.panTo([50.5, 30.5]);
-})
-.error(function(err) {
-  console.log(err);
-});
+	cartodb.createVis('map', 'https://rtapia.cartodb.com/api/v2/viz/d2673aee-f9a3-11e4-a29c-0e9d821ea90d/viz.json', {
+		//shareable: true,
+		title: true,
+		description: true,
+		//search: true,
+		tiles_loader: true,
+		center_lat: 19.411,
+		center_lon: -99.16,
+		zoom: 16
+	})
+	.done(function(vis, layers) {
+	  // layer 0 is the base layer, layer 1 is cartodb layer
+	  // setInteraction is disabled by default
+	  /*layers[0].setInteraction(true);
+	  layers[0].on('featureOver', function(e, latlng, pos, data) {
+		cartodb.log.log(e, latlng, pos, data);
+	  });*/
+	  // you can get the native map to work with it
+	  var map = vis.getNativeMap();
+	  // now, perform any operations you need
+	  // map.setZoom(3);
+	  // map.panTo([50.5, 30.5]);
+	})
+	.error(function(err) {
+	  console.log(err);
+	});
 
-var sql = new cartodb.SQL({ user: 'rtapia' });
-sql.execute("SELECT categoria, COUNT(*) FROM levantamiento_final GROUP BY categoria")
-// para la evolucion del radar por dia
-//"SELECT DISTINCT fecha::date, categoria, COUNT(categoria) OVER (PARTITION BY fecha::date) FROM levantamiento_final ORDER BY fecha::date"
-  .done(function(data) {
-	//console.log(data.rows);
-	hazRadar(data.rows);
-  })
-  .error(function(errors) {
-	// errors contains a list of errors
-	console.log("errors:" + errors);
-  })
-
+	var sql = new cartodb.SQL({ user: 'rtapia' });
+	sql.execute("SELECT categoria, COUNT(*) FROM levantamiento_final GROUP BY categoria")
+	// para la evolucion del radar por dia
+	//"SELECT DISTINCT fecha::date, categoria, COUNT(categoria) OVER (PARTITION BY fecha::date) FROM levantamiento_final ORDER BY fecha::date"
+	.done(function(data) {
+		//console.log(data.rows);
+		hazRadar(data.rows);
+	})
+	.error(function(errors) {
+		// errors contains a list of errors
+		console.log("errors:" + errors);
+	})
 }
 window.onload = main;
 
